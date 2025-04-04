da285af3220743cd38e6365bb73731bf6ee5a709
Iliya Raev <insomniq4@abv.bg>
2025-04-04 18:09:01 +0300

    Update Library class: add validations for name, address, and numberOfEmployees

--- src/Library.java
+++ src/Library.java
@@ -7,26 +7,58 @@ public class Library {
     private int numberOfEmployees;
     private final List<Book> listOfBooks = new ArrayList<>();
 
-    Library(String libraryName, String address, int numberOfEmployees, Book currentBook){
+    Library(String libraryName, String address, int numberOfEmployees) {
         this.libraryName = libraryName;
         this.address = address;
         this.numberOfEmployees = numberOfEmployees;
-        this.listOfBooks.add(currentBook);
     }
 
-    public int getNumberOfEmployees(){
+    public static boolean checkLibraryName(String libraryName) {
+        if(libraryName == null || libraryName.isBlank()){
+            System.out.println("Library name cannot be empty.");
+            return false;
+        }
+        if(libraryName.length() > 100){
+            System.out.println("The library name cannot be longer than 100 characters.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkAddress(String address){
+        if(address.isBlank()){
+            System.out.println("Address cannot be empty");
+            return false;
+        }
+        if(address.length() > 500){
+            System.out.println("The address cannot be longer than 500 characters.");
+            return false;
+
+        }
+        return true;
+    }
+
+    public static boolean checkNumberOfEmployees(int numberOfEmployees){
+        if(numberOfEmployees <= 0 || numberOfEmployees >= 50){
+            System.out.println("The number of library employees must be greater than 0 and less than 10! Please try again!");
+            return false;
+        }
+        return true;
+    }
+
+    public int getNumberOfEmployees() {
         return numberOfEmployees;
     }
 
-    public void setNumberOfEmployees(int numberOfEmployees){
+    public void setNumberOfEmployees(int numberOfEmployees) {
         this.numberOfEmployees = numberOfEmployees;
     }
 
-    public List<Book> getListOfBooks(){
+    public List<Book> getListOfBooks() {
         return listOfBooks;
     }
 
-    public void setListOfBooks(Book book){
+    public void setListOfBooks(Book book) {
         this.listOfBooks.add(book);
     }
 }
